e287b75becf437790f87f8fe3e9cf63051bc0255
rgraue <rgraue@outlook.com>
2025-05-05 22:30:21 -0700

    maybe one of the best things ive made

--- src/translator.ts
+++ src/translator.ts
@@ -1,18 +1,84 @@
 import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
 
 const app = express();
+
+app.use(express.json());
+
+app.use(cors());
 
 const args = process.argv;
 
 const PORT = args[2] || 8081
 const TARGET = args[3] || 9000
 
-app.post(/api/, (req, res) => {
-    console.log(req);
-    console.log(PORT);
-    console.log(TARGET);
-    res.send('hello');
+export const sendToLocalDockerLambda = async (payload: any) => {
+    const result = await axios.post(`http://localhost:${TARGET}/2015-03-31/functions/function/invocations`, payload);
+
+    return await result.data;
+}
+
+
+app.post(/api/, async (req, res, next) => {
+
+    const {headers, body, url} = req;
+    console.log(`POST::${url}`);
+
+    const translated = {
+        body,
+        headers,
+        requestContext: {
+            http: {
+                method: 'POST',
+                path: url
+            }
+        }
+    }
+
+    const result = await sendToLocalDockerLambda(translated);
+
+    res.send(result);
 });
+
+app.put(/api/, async (req, res, next) => {
+    const {headers, body, url} = req;
+    console.log(`PUT::${url}`);
+
+    const translated = {
+        body,
+        headers,
+        requestContext: {
+            http: {
+                method: 'PUT',
+                path: url
+            }
+        }
+    }
+
+    const result = await sendToLocalDockerLambda(translated);
+
+    res.send(result);
+})
+
+app.get(/api/, async (req, res, next) => {
+    const {headers, url} = req;
+    console.log(`GET::${url}`);
+
+    const translated = {
+        headers,
+        requestContext: {
+            http: {
+                method: 'GET',
+                path: url
+            }
+        }
+    }
+
+    const result = await sendToLocalDockerLambda(translated);
+
+    res.send(result);
+})
 
 app.listen(PORT, () => {
     console.log(`translator running on ${PORT}`)
